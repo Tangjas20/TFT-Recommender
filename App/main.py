68185027589159d7918844bfa44d756aaedc9886
Tangjas20 <tangjas2020@gmail.com>
2022-01-08 00:34:47 +1100

    Updated Tkinter to have screenshot and quit

--- App/main.py
+++ App/main.py
@@ -15,11 +15,15 @@ class Window(tk.Frame):                           # <-- avoid star imports
         self.pack(fill=tk.BOTH, expand=True)
 
         self.quit_button = tk.Button(self, text='Quit', command=self.master.destroy)
-        self.quit_button.pack()
+        self.quit_button.pack(side = tk.BOTTOM, pady = 20)
+        self.screenshot_button = tk.Button(self, text="Take screenshot")
+       # self.screenshot_button.grid(column=4)
+      #  self.screenshot_button.place(x = 50, y = 200)
+        self.screenshot_button.pack(padx = 50, pady=20)
+      #  self.screenshot_button.pack()
 
 
 if __name__ == '__main__':
-
     root = tk.Tk()
     app = Window(root)
     root.mainloop()
